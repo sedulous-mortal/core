143c92801309565be3e70945530cd66b39f036f2
Alison Stuart (sedulous-mortal) <alisonnicolestuart@gmail.com>
2025-01-04 22:31:35 -0500

    terrible problems with the proxying, tried loads of stack solutions and nothing worked, starting over with a prebundled kit from shipyard

--- core-flask-backend/app.py
+++ core-flask-backend/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, send_from_directory
+import datetime
 
-app = Flask(__name__, static_folder='my-react-app/build', static_url_path='/')
+x = datetime.datetime.now()
 
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
+# app = Flask(__name__, static_folder='my-react-app/build', static_url_path='/')
+app = Flask(__name__)
+
+#Route for homepage
+# @app.route('/')
+# def index():
+#     return send_from_directory(app.static_folder, 'index.html')
+
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+
+    # Returning an api for showing in  reactjs
+    return {
+        'Name':"geek", 
+        "Age":"22",
+        "Date":x, 
+        "programming":"python"
+        }
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- core-react-frontend/src/App.js
+++ core-react-frontend/src/App.js
@@ -1,26 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    // usestate for setting a javascript
+    // object for storing and using data
+    const [data, setdata] = useState({
+        name: "",
+        age: 0,
+        date: "",
+        programming: "",
+    });
+
+    // Using useEffect for single rendering
+    useEffect(() => {
+        // Using fetch to fetch the api from 
+        // flask server it will be redirected to proxy
+        fetch("/data").then((res) =>
+            res.json().then((data) => {
+                // Setting a data from api
+                setdata({
+                    name: data.Name,
+                    age: data.Age,
+                    date: data.Date,
+                    programming: data.programming,
+                });
+            })
+        );
+    }, []);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> */}
+        </header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to CORE.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>Welcome to CORE.</p>
+        
+        <h1>Sample Profile:</h1>
+        {/* Calling a data from setdata for showing */}
+        {/* <p>{data.name}</p>
+        <p>{data.age}</p>
+        <p>{data.date}</p>
+        <p>{data.programming}</p> */}
+      </div>
+    );
 }
 
 export default App;
